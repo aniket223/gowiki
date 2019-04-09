f4c04ed9bc85a8c612dd51d9964aaf84e0cd9f3a
aniket223 <aniket88885@gmail.com>
2019-04-09 17:55:17 +0530

    adding load and save functions

--- wiki.go
+++ wiki.go
@@ -1,10 +1,27 @@
 package main
 
-//import different go packages
+import ( //import different go packages
 
-type Page struct {
+	"io/ioutil"
+)
+
+type Page struct { //struct of pages
 	Title string
 	Body  []byte
+}
+
+func (p *Page) save() error { //function for saving pages
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600) //simple library writing to files
+}
+
+func loadPage(title string) (*Page, error) { //function for loading pages
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename) // 2 arguments to check if error occurs
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
 
 func main() {
