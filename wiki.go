bd5d6229e5961af8a065eb63979ce1ee0cc2b83a
aniket223 <aniket88885@gmail.com>
2019-04-10 15:47:38 +0530

    adding saveHandler function

--- wiki.go
+++ wiki.go
@@ -51,9 +51,18 @@ func editHandler(w http.ResponseWriter, r *http.Request) { //for editing
 	renderTemplate(w, "edit", p)
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request) { //for saving pages
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+
+}
+
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	//http.HandleFunc("/save/", saveHandler)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8082", nil))
 }
